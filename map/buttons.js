d3b2f541ae6642fbb456f5c23f67c851edb5f284
source-chunk <duosource.iron@gmail.com>
2019-07-09 21:47:47 -0500

    inital commit

---
+++ map/buttons.js
@@ -0,0 +1,247 @@
+// Randomly roll one of the potential (numbered) chunks
+function pickPotentialChunk() {
+    // Get checkbox values
+    var removePotential = document.getElementById("removePotential").checked;
+    var selectNewNeighbors = document.getElementById("selectNewNeighbors").checked;
+	var chunks = document.getElementsByClassName("potential");
+	if (chunks.length == 0) return;
+
+    removeUnlockedTileNumbers();
+    removeBetweenTileNumbers();
+
+	// Randomly pick one of the numbered tiles
+    var randomIndex = Math.floor(Math.random() * chunks.length);
+	for (var i = chunks.length - 1; i >= 0; i--) {
+		if (i == randomIndex) {
+            var chunk = chunks[i];
+            // Set new neighbors as potential
+            if (selectNewNeighbors && !removePotential) {
+                addPotentialNeighborsForID(chunks[randomIndex].id);
+            }
+            chunk.className = "between";
+            removeElementFromArray(potentialChunks, Number(chunk.id));
+			// Wait 1 second for "between" animation to finish
+			setTimeout(() => {
+				//var savedText = chunk.innerText;
+				addChunkAsUnlocked(chunk.id);
+				//chunk.innerText = savedText;
+            }, 1000);
+		}
+		else if (removePotential) {
+            addChunkAsLocked(chunks[i].id);
+        }
+    }
+    
+    if (!removePotential) {
+        updatePotentialNumbers();
+    }
+}
+
+function pickChunk(id) {
+    // Get checkbox values
+    var removePotential = document.getElementById("removePotential").checked;
+    var selectNewNeighbors = document.getElementById("selectNewNeighbors").checked;
+	var chunks = document.getElementsByClassName("maybe");
+	if (chunks.length == 0) return;
+
+    removeUnlockedTileNumbers();
+    removeBetweenTileNumbers();
+
+	// Randomly pick one of the numbered tiles
+    var randomIndex = id;
+	for (var i = chunks.length - 1; i >= 0; i--) {
+		if (chunks[i] !== undefined && chunks[i].id == randomIndex) {
+            var chunk = chunks[i];
+            // Set new neighbors as potential
+            if (selectNewNeighbors && !removePotential) {
+                addPotentialNeighborsForID(chunks[i].id);
+            }
+            chunk.className = "between";
+            removeElementFromArray(maybeChunks, Number(chunk.id));
+			// Wait 1 second for "between" animation to finish
+			setTimeout(() => {
+				//var savedText = chunk.innerText;
+				addChunkAsUnlocked(chunk.id);
+				//chunk.innerText = savedText;
+            }, 1000);
+		}
+		else {
+            addChunkAsPotential(chunks[i].id);
+        }
+    }
+    
+    if (!removePotential) {
+        updatePotentialNumbers();
+    }
+}
+
+// Randomly roll two
+function pickPotentialChunk2() {
+    // Get checkbox values
+    var removePotential = document.getElementById("removePotential").checked;
+    var selectNewNeighbors = document.getElementById("selectNewNeighbors").checked;
+	var chunks = document.getElementsByClassName("potential");
+	if (chunks.length == 0) return;
+
+    removeUnlockedTileNumbers();
+    removeBetweenTileNumbers();
+
+	// Randomly pick one of the numbered tiles
+	for (var ii = 0; ii < 2; ii++) {
+    var randomIndex = Math.floor(Math.random() * chunks.length);
+	for (var i = chunks.length - 1; i >= 0; i--) {
+		if (i == randomIndex) {
+			var chunk = chunks[i];
+			chunk.className = "between";
+			removeElementFromArray(potentialChunks, Number(chunk.id));
+			//var savedText = chunk.innerText;
+			addChunkAsMaybe(chunk.id);
+			//chunk.innerText = savedText;
+		}
+		else if (removePotential) {
+            addChunkAsLocked(chunks[i].id);
+        }
+    }
+	}
+    
+    if (!removePotential) {
+        updatePotentialNumbers();
+    }
+}
+
+// Move the map to the center point between all unlocked tiles
+function centerOnUnlockedTiles() {
+    var centerPoint = [0, 0];
+    for (var i = 0; i < unlockedChunks.length; i++) {
+        // Sum all unlocked chunk center points, then divide by the number of unlocked chunks
+        var chunkPoint = getChunkCenterPoint(unlockedChunks[i]);
+        centerPoint[0] += chunkPoint[0];
+        centerPoint[1] += chunkPoint[1];
+    }
+    centerPoint[0] /= unlockedChunks.length;
+    centerPoint[1] /= unlockedChunks.length;
+
+    repositionMapOnPoint(document.getElementById("imgDiv"), centerPoint[0], centerPoint[1]);
+
+    fixMapEdges(document.getElementById("imgDiv"));
+}
+
+// Disable the other checkbox when one is active
+function disableOtherCheckbox(box) {
+    if (box.id == "selectNewNeighbors" && box.checked) {
+        document.getElementById("removePotential").disabled = true;
+    }
+    else if (box.id == "selectNewNeighbors" && !box.checked) {
+        document.getElementById("removePotential").disabled = false;
+    }
+    else if (box.id == "removePotential" && box.checked) {
+        document.getElementById("selectNewNeighbors").disabled = true;
+    }
+    else if (box.id == "removePotential" && !box.checked) {
+        document.getElementById("selectNewNeighbors").disabled = false;
+    }
+}
+
+// Return the X- and Y-values of a given chunk's center point
+function getChunkCenterPoint(id) {
+    var chunk = document.getElementById(id);
+    // Format: [x, y]
+    var point = [
+        chunk.offsetWidth * (id % cols) + chunk.offsetWidth / 2,
+        chunk.offsetHeight * Math.floor(id / cols) + chunk.offsetHeight / 2
+    ];
+    return point;
+}
+
+// Set all neighbors to this chunk as potential
+function addPotentialNeighborsForID(id) {
+    id = Number(id);
+
+    var newNeighbors = getAdjacentTileIDs(id);
+    for (var i = 0; i < newNeighbors.length; i++) {
+        if (newNeighbors[i] == -1) continue;
+        else if (document.getElementById(newNeighbors[i]).className == "locked") {
+            addChunkAsPotential(newNeighbors[i]);
+        }
+    }
+}
+
+// TODO: Add button for this
+// Select all chunks neighboring "unlocked" chunks
+function selectAllNeighbors() {
+    for (var i = 0; i < unlockedChunks.length; i++) {
+        addPotentialNeighborsForID(unlockedChunks[i]);
+    }
+}
+
+// Toggle visibility of a given sidebar
+function toggleSidebar(id, side) {
+    var sideDiv = document.getElementById(side);
+    // Slide the sidebar off the screen
+    if (getComputedStyle(sideDiv).getPropertyValue("display") == "block") {
+        // Pick between the left or right sidebar
+        if (side == "sidebarLeft") {
+            sideDiv.style.left = -(sideDiv.offsetWidth) + "px";
+        }
+        else if (side == "sidebarRight") {
+            sideDiv.style.right = (-sideDiv.offsetWidth) + "px";
+        }
+        // Wait until the sidebar is off-screen to make it invisible
+        // Then fix the map if it is now out of bounds
+        setTimeout(() => {
+            sideDiv.style.display = "none";
+            fixMapEdges(document.getElementById("imgDiv"));
+        }, 750);
+    }
+    // Slide the sidebar back on the screen
+    else {
+        sideDiv.style.display = "block";
+        // Wait a small amount before starting the animation because
+        // Firefox sometimes doesn't show the animation
+        setTimeout(() => {
+            if (side == "sidebarLeft") {
+                sideDiv.style.left = "0%";
+            }
+            else if (side == "sidebarRight") {
+                sideDiv.style.right = "0%";
+            }
+        }, 15);
+    }
+
+    var arrowBackground = document.getElementById(id + "Background");
+    var arrow = document.getElementById(id);
+    // Slide and rotate the arrow to the edge of the screen
+    setTimeout(() => {
+        
+    }, 15);
+    if (id == "arrowLeft") {
+        // Custom left value hasn't been set yet, so initialize it
+        if (arrowBackground.style.left == "") arrowBackground.style.left = "13.5%";
+
+        if (arrowBackground.style.left == "13.5%") {
+            arrowBackground.style.left = "-0.5%";
+            arrow.style.transform = "rotate(180deg)";
+        }
+        else if (arrowBackground.style.left == "-0.5%") {
+            setTimeout(() => {
+                arrowBackground.style.left = "13.5%";
+                arrow.style.transform = "rotate(0deg)";
+            }, 15);
+        }
+    }
+    else if (id == "arrowRight") {
+        // Custom right value hasn't been set yet, so initialize it
+        if (arrowBackground.style.right == "") arrowBackground.style.right = "13.5%";
+
+        if (arrowBackground.style.right == "13.5%") {
+            arrowBackground.style.right = "-0.5%";
+            arrow.style.transform = "rotate(-180deg)";
+        }
+        else if (arrowBackground.style.right == "-0.5%") {
+            setTimeout(() => {
+                arrowBackground.style.right = "13.5%";
+                arrow.style.transform = "rotate(0deg)";
+            }, 15);
+        }
+    }
+}
---
+++ map/map.js
@@ -0,0 +1,206 @@
+var rows = 25;
+var cols = 43;
+var dragging = false;
+var unlockedChunks = [];
+var potentialChunks = [];
+var maybeChunks = [];
+var borderWidth = " 2px";
+
+// Build an array of divs that act like buttons
+function buildArray(rows, columns) {
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < columns; j++) {
+            var btn = document.createElement("div");
+			btn.className = "locked";
+			btn.style.fontSize = "50pt";
+            btn.id = (i * columns + j).toString();
+            btn.setAttribute("onclick", "toggleChunkButton(this.id)");
+            document.getElementById("btnDiv").appendChild(btn);
+        }
+    }
+}
+
+// Toggle chunk type on click
+function toggleChunkButton(id) {
+	if (dragging == true) {
+		// Prevent clicking when dragging the map
+		dragging = false;
+	}
+	else {
+		var btn = document.getElementById(id);
+		if (btn.className == "locked") {
+			addChunkAsPotential(id);
+		}
+		else if (btn.className == "potential") {
+			btn.innerText = "";
+			addChunkAsUnlocked(id);
+		}
+		else if (btn.className == "unlocked") {
+			addChunkAsLocked(id);
+		}
+		else if (btn.className == "maybe") {
+			pickChunk(id);
+		}
+	}
+}
+
+function addChunkAsPotential(id) {
+	id = Number(id); // For caution
+	var btn = document.getElementById(id);
+
+	if (potentialChunks.indexOf(id) == -1) {
+		potentialChunks.push(id);
+	}
+
+	removeUnlockedTileNumbers();
+	updatePotentialNumbers();
+	btn.className = "potential";
+}
+
+function addChunkAsUnlocked(id) {
+	id = Number(id);
+	var btn = document.getElementById(id);
+	btn.className = "unlocked";
+	btn.innerText = "";
+
+	potentialChunks = removeElementFromArray(potentialChunks, id);
+	if (unlockedChunks.indexOf(id) == -1) {
+		unlockedChunks.push(id);
+	}
+
+	updatePotentialNumbers();
+	drawUnlockedBorders();
+}
+
+function addChunkAsMaybe(id) {
+	id = Number(id);
+	var btn = document.getElementById(id);
+	btn.className = "maybe";
+	btn.innerText = "";
+	
+	potentialChunks = removeElementFromArray(potentialChunks, id);
+	if (maybeChunks.indexOf(id) == -1) {
+		maybeChunks.push(id);
+	}
+
+	updatePotentialNumbers();
+	drawUnlockedBorders();
+}
+
+function addChunkAsLocked(id) {
+	id = Number(id);
+	var btn = document.getElementById(id);
+	btn.className = "locked";
+	btn.innerText = "";
+
+	// Remove from both arrays instead of just "unlockedChunks"
+	// This is needed because of the Import/Export function
+	potentialChunks = removeElementFromArray(potentialChunks, id);
+	unlockedChunks = removeElementFromArray(unlockedChunks, id);
+
+	// This chunk is no longer unlocked, update borders
+	drawUnlockedBorders();
+}
+
+// Return 4 integers that represent neighboring chunk IDs
+// If the given chunk is on an edge, return -1 for that direction
+function getAdjacentTileIDs(id) {
+	id = Number(id);
+	// Format: top, right, bottom, left
+    var neighbors = [];
+
+    var top = false;
+    var right = false;
+    var bottom = false;
+    var left = false;
+
+    // Check if this chunk is on an edge
+    if (0 <= id && id < cols) top = true;
+    if ((id + 1) % cols == 0) right = true;
+    if ((rows - 1) * cols <= id && id < rows * cols) bottom = true;
+    if (id % cols == 0) left = true;
+    
+    // Top neighbor
+	if (!top) neighbors.push(Number(id) - cols);
+    else neighbors.push(-1); // Edge case, return -1 for this direction
+    // Right neighbor
+    if (!right) neighbors.push(Number(id) + 1);
+    else neighbors.push(-1);
+    // Bottom neighbor
+    if (!bottom) neighbors.push(Number(id) + cols);
+    else neighbors.push(-1);
+    // Left neighbor
+    if (!left) neighbors.push(Number(id) - 1);
+    else neighbors.push(-1);
+
+    return neighbors;
+}
+
+// Draw borders around a given tile, with no borders between unlocked tiles
+function makeBorders(id) {
+	var neighbors = getAdjacentTileIDs(parseInt(id));
+	var chunk = document.getElementById(id);
+
+	var borderString = "";
+
+	for (var i = 0; i < neighbors.length; i++) {
+		// Check existence of each neighbor
+		if (neighbors[i] > -1) {
+			if (document.getElementById(neighbors[i]).className == "unlocked") {
+				// Don't draw borders on neighboring unlocked tiles
+				borderString = borderString + " 0px";
+			}
+			else {
+				borderString = borderString + borderWidth;
+			}
+		}
+		else {
+			borderString = borderString + borderWidth;
+		}
+	}
+
+	chunk.style.borderWidth = borderString;
+}
+
+// Remove the number in unlocked chunks that still have their number from random selection
+function removeUnlockedTileNumbers() {
+	var unlocked = document.getElementsByClassName("unlocked");
+    
+    for (var i = 0; i < unlocked.length; i++) {
+        unlocked[i].innerText = "";
+	}
+}
+	
+// Remove numbers on "between" tiles if the user is spam-clicking the picker
+function removeBetweenTileNumbers() {
+	var betweens = document.getElementsByClassName("between");
+
+	for (var i = 0; i < betweens.length; i++) {
+		betweens[i].innerText = "";
+	}
+}
+
+// Draw borders around all unlocked chunks, with no borders between other unlocked chunks
+function drawUnlockedBorders() {
+    var unlocked = document.getElementsByClassName("unlocked");
+
+    for (var i = 0; i < unlocked.length; i++) {
+        makeBorders(unlocked[i].id);
+    }
+}
+
+// Update numbers so that all numbers are unique and don't skip any
+function updatePotentialNumbers() {
+	for (var i = 0; i < potentialChunks.length; i++) {
+		document.getElementById(potentialChunks[i]).innerText = i + 1;
+	}
+}
+
+// Remove an element from a given array if the element exists
+function removeElementFromArray(array, element) {
+	var index = array.indexOf(element);
+	if (index > -1) {
+		array.splice(index, 1);
+	}
+	return array;
+}
